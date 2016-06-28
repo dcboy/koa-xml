359f611dca6730e58a326fcce1b52f6d98d3acd1
DC <dc@sunlight-tech.com>
2016-06-28 17:14:09 +0800

    add text/xml support

--- index.js
+++ index.js
@@ -3,18 +3,11 @@
 const parse = require('./lib/xml')
 
 module.exports = (options) => {
-  return function * (next) {
-    const ctx = this
-    if (ctx.is('xml')) {
-      switch (ctx.method) {
-        case 'POST':
-        case 'PUT':
-        case 'PATCH':
-        case 'TRACE':
-        ctx.request.body = yield parse(ctx.req, options)
-        break
-      }
+    return function*(next) {
+        const ctx = this
+        if (ctx.is('text/xml', 'xml') && /^(POST|PUT|PATCH)$/i.test(ctx.method)) {
+            ctx.request.body = yield parse(ctx.req, options);
+        }
+        yield next
     }
-    yield next
-  }
 }
