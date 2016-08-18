f0bd9e76d49d1ebf0d84b1f7d5497f89c11d5a81
DC <dc@sunlight-tech.com>
2016-08-18 19:03:40 +0800

    fixed TRACE method support

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const parse = require('./lib/xml')
 module.exports = (options) => {
     return function*(next) {
         const ctx = this
-        if (ctx.is('text/xml', 'xml') && /^(POST|PUT|PATCH)$/i.test(ctx.method)) {
+        if (ctx.is('text/xml', 'xml') && /^(POST|PUT|PATCH|TRACE)$/i.test(ctx.method)) {
             ctx.request.body = yield parse(ctx.req, options);
         }
         yield next
